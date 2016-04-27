52ab339b9d8bfffd9159444b6e2096e538275a91
Raphael Marco <raphaelmarco@outlook.com>
2016-04-27 12:38:36 +0800

    Added license

--- schema.sql
+++ schema.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Apr 27, 2016 at 03:30 AM
--- Server version: 5.6.30-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `online_grades`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admins`
---
-
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -36,12 +8,6 @@ CREATE TABLE IF NOT EXISTS `admins` (
   `department` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `faculties`
---
 
 CREATE TABLE IF NOT EXISTS `faculties` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -54,12 +20,6 @@ CREATE TABLE IF NOT EXISTS `faculties` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `grades`
---
-
 CREATE TABLE IF NOT EXISTS `grades` (
   `student_id` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
   `subject` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
@@ -71,24 +31,12 @@ CREATE TABLE IF NOT EXISTS `grades` (
   PRIMARY KEY (`student_id`,`subject`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `sessions`
---
-
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` varbinary(128) NOT NULL,
   `data` mediumblob NOT NULL,
   `expiry` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
 
 CREATE TABLE IF NOT EXISTS `students` (
   `id` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
@@ -99,6 +47,5 @@ CREATE TABLE IF NOT EXISTS `students` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `admins` (`id`, `username`, `password`, `first_name`, `middle_name`, `last_name`, `department`) VALUES
+(1, 'admin', '$2y$10$if/foP.Q2qWOcfI5P5cvme64KJzWXXnAlfzVrLMrANqZQ0e9yD.XW', 'STI', 'Novaliches', 'Admin', 'Information Technology Head');
