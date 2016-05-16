e9409f0974f795e7a9bd2e4fbbbfac89eb249e89
Raphael Marco <raphaelmarco@outlook.com>
2016-05-16 23:19:57 +0800

    Ignore duplicate faculties on import

--- app/Components/Importer/FacultyImporter.php
+++ app/Components/Importer/FacultyImporter.php
@@ -32,7 +32,8 @@ class FacultyImporter implements ImporterInterface
             'HR'    => 'Hotel and Restaurant Management',
             'TM'    => 'Hotel and Restaurant Management',
             'TH'    => 'Hotel and Restaurant Management',
-            'HR/TM' => 'Hotel and Restaurant Management'
+            'HR/TM' => 'Hotel and Restaurant Management',
+            'GE'    => 'General Education'
         );
 
         $departments = Department::all()->groupBy(function (Department $department) {
@@ -47,16 +48,21 @@ class FacultyImporter implements ImporterInterface
                     $departmentId = $departments->get($mappings[$data['department']])[0]->id;
                 }
 
-                $input[] = array(
+                $facultyIdentifer = array(
                     'last_name'     => $data['last_name'],
                     'first_name'    => $data['first_name'],
-                    'middle_name'   => $data['middle_name'],
-                    'department_id' => $departmentId,
-                    'username'      => strtoupper(substr($data['first_name'], 0, 1) . $data['last_name']),
-                    'password'      => Hash::make('stinova123'),
-                    'created_at'    => date('Y-m-d H:i:s'),
-                    'updated_at'    => date('Y-m-d H:i:s')
+                    'middle_name'   => $data['middle_name']
                 );
+
+                if (Faculty::where($facultyIdentifer)->count() == 0) {
+                    $input[] = array_merge($facultyIdentifer, array(
+                        'department_id' => $departmentId,
+                        'username'      => strtoupper(substr($data['first_name'], 0, 1) . $data['last_name']),
+                        'password'      => Hash::make('stinova123'),
+                        'created_at'    => date('Y-m-d H:i:s'),
+                        'updated_at'    => date('Y-m-d H:i:s')
+                    ));
+                }
             }
         }
 
